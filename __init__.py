e48a18e7a8d460b606d8960387a724ab9a18676b
Daniel <daniel.ramos.rodrig@gmail.com>
2025-11-01 13:45:47 +0100

    refactor: Migrate to dbus-fast.

--- __init__.py
+++ __init__.py
@@ -1,67 +1,47 @@
 
 import os
 import sys
+import asyncio
 from aqt import mw
 from aqt.operations import QueryOp
 from aqt.theme import theme_manager
 
-# Add vendor dir to path so that dasbus is importable.
+# Add vendor dir to path so that dbus_fast is importable.
 vendor_dir = os.path.join(os.path.dirname(__file__), "vendor")
 sys.path.insert(0, vendor_dir)
 
-# This will get replaced on postInstall to make sure PyGObject is found.
-# NIX_PYTHON_PATH_INJECTION
+from dbus_fast.aio import MessageBus
 
-try:
-    # This import will fail if the user doesn't have PyGObject installed
-    from gi.repository import GLib
+async def bus_listener():
 
-    from dasbus.connection import SessionMessageBus
-    from dasbus.loop import EventLoop
-
-    def bus_listener_thread():
-
-        bus = SessionMessageBus()
-        loop = EventLoop()
-
-        proxy = bus.get_proxy(
-            "org.freedesktop.impl.portal.Settings",   # Bus name
-            "/org/freedesktop/portal/desktop",        # Object path
-        )
-
-        def callback(path, setting, value):
-            # Reload theme if the light/dark mode property has changed value.
-            if path == "org.freedesktop.appearance" and setting == "color-scheme":
-                aqt.mw.taskman.run_on_main(
-                    lambda: theme_manager.apply_style()
-                )
-
-        proxy.SettingChanged.connect(callback)
-
-        loop.run()
-
-    dbus_op = QueryOp(
-        parent = mw,
-        op=lambda _: bus_listener_thread()
+    bus = await MessageBus().connect()
+    introspection = await bus.introspect(
+        "org.freedesktop.portal.Desktop",
+        "/org/freedesktop/portal/desktop"
     )
 
-    dbus_op.run_in_background()
+    obj = bus.get_proxy_object(
+        "org.freedesktop.portal.Desktop",
+        "/org/freedesktop/portal/desktop",
+        introspection
+    )
+    settings = obj.get_interface("org.freedesktop.portal.Settings")
 
-except ImportError:
+    def callback(path, setting, value):
+        # Reload theme if the light/dark mode property has changed value.
+        if path == "org.freedesktop.appearance" and setting == "color-scheme":
+            aqt.mw.taskman.run_on_main(
+                lambda: theme_manager.apply_style()
+            )
 
-    from aqt import gui_hooks
-    from aqt.utils import showWarning
+    settings.on_setting_changed(callback)
 
-    def displayDependencyWarning():
-        msg = """
-            <b>Anki Dark Mode Fix failed to load</b><br><br>
-            This add-on requires system libraries for D-Bus communication and will only work on Linux.
-            <br><br>
-            Please install the Python GI bindings using your system's package manager.
-            <br><br>
-            After installing, please restart Anki.
-        """
-        showWarning(msg, parent=mw, textFormat="rich")
+    await asyncio.Event().wait()
 
-    gui_hooks.main_window_did_init.append(displayDependencyWarning)
+dbus_op = QueryOp(
+    parent = mw,
+    op=lambda _: asyncio.run(bus_listener())
+)
+
+dbus_op.run_in_background()
 
--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     let
         system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
-        package = { anki-utils, nix-update-script, pygobject3 }: anki-utils.buildAnkiAddon rec {
+        package = { anki-utils, nix-update-script }: anki-utils.buildAnkiAddon {
             pname = "ankidarkmodefix";
             version = "1.0";
             src = pkgs.lib.fileset.toSource {
@@ -19,19 +19,6 @@
                 ];
             };
             passthru.updateSript = nix-update-script { };
-            buildInputs = [ pygobject3 ];
-            postInstall = ''
-                # The path to the file we need to modify
-                target_file="$out/$installPrefix/__init__.py"
-
-                # The Python code we want to inject.
-                # It adds a list of paths to the beginning of sys.path.
-                injection_code="import sys; sys.path = [p for p in '${pkgs.python3.pkgs.makePythonPath buildInputs}'.split(':') if p] + sys.path"
-
-                # Use sed to replace our placeholder with the real code.
-                # The '@' symbols are used as delimiters for sed to avoid conflicts with '/' in paths.
-                sed -i "s@# NIX_PYTHON_PATH_INJECTION@$injection_code@" "$target_file"
-            '';
         };
     in
     {
@@ -39,11 +26,10 @@
             packages = with pkgs; [
                 python313
                 python313Packages.pip
-                python313Packages.pygobject3
             ];
         };
 
-        packages."${system}".default = pkgs.callPackage package { pygobject3 = pkgs.python313Packages.pygobject3; };
+        packages."${system}".default = pkgs.callPackage package { };
     };
 }
 
